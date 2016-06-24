0a3880c4931540f7f7e12449d224730c603a2905
Scorpio <scorpiocoffee91@gmail.com>
2016-06-24 13:03:04 +0100

    s

--- client/pages/app.js
+++ client/pages/app.js
@@ -1,6 +1,5 @@
 'use strict';
 
-/** Provide the routes and service of socket.io */
 angular.module('webApp', [
   'ngCookies',
   'ngResource',
@@ -17,36 +16,9 @@ angular.module('webApp', [
         templateUrl: 'pages/profile/profile.html',
         controller: 'ProfileController'
       })
-      .when('/words', {
-        templateUrl: 'pages/words/words.html',
-        controller: 'WordsController'
-      })
       .otherwise({
         redirectTo: '/'
       });
 
     $locationProvider.html5Mode(true);
-})
-  .factory('socket',function($rootScope) {
-    var socket = io();
-    return {
-      on: function(eventName, callback) {
-        socket.on(eventName, function() {
-          var args = arguments;
-          $rootScope.$apply(function() {
-            callback.apply(socket, args);
-          });
-        });
-      },
-      emit: function(eventName, data, callback) {
-        socket.emit(eventName, data, function() {
-          var args = arguments;
-          $rootScope.$apply(function() {
-            if(callback) {
-              callback.apply(socket, args);
-            }
-          });
-        });
-      }
-    };
-  });
+});
--- index.js
+++ index.js
@@ -3,14 +3,12 @@
 var http = require('http');
 var express = require('express');
 var bodyParser = require('body-parser');
+var errorhandler = require('errorhandler');
 var path = require('path');
 var app = express();
-var errorhandler = require('errorhandler');
 var server = require('http').createServer(app);
 var io = require('socket.io')(http);
 var socket = io.listen(server);
-var twitter = require('twit');
-var hotwords = require('./api/words_parse');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -20,7 +18,8 @@ app.get('/*', function(req, res) {
 	res.sendFile(__dirname + '/client/index.html');
 });
 
-server.listen(3000, function() {
-   console.log('Express server listening on %d, in %s mode', 3000, app.get('env'));
+var PORT = process.env.PORT || 3000;
+server.listen(PORT, function() {
+   console.log('Express server listening on %d, in %s mode', 3000);
 });
-
+exports = module.exports = app;
